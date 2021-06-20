e6cf223bdc165ecb34529efa59878d46c965665a
aman123956 <you@example.com>
2021-06-20 15:00:33 +0530

    Basic Layout Done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './styles.module.css'
 import Navbar from '../Navbar/index'
+import Modals from '../Modal/index'
 
 function Home() {
     return (
@@ -10,23 +11,30 @@ function Home() {
             {/* Main Content(20:80) */}
             <div className="my-2 d-flex justify-content-flex-start">
                 {/* Left Part(20%) */}
-                <div className="w-40 d-flex flex-column align-items-center p-4 border border-primary">
+                <div className={styles.leftSideBar}>
+                    {/* Add File/Folder */}
+                    <Modals />
+                    <br />
                     <button className={styles.optionBtn}>
                         My Drive
                     </button>
                     <button className={styles.optionBtn}>
-                        My Drive
+                        Computers
                     </button>
                     <button className={styles.optionBtn}>
-                        My Drive
+                        Shared with me
                     </button>
                      <button className={styles.optionBtn}>
-                        My Drive
+                        Recent
                     </button>
                     <button className={styles.optionBtn}>
-                        My Drive
+                        Starred
                     </button>
                 </div>
+            </div>
+            {/* Right Part */}
+            <div className={styles.rightBox}>
+                {/* Files To Show */}
             </div>
         </div>
     )
