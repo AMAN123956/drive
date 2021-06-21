4b50b08fc00406ef5fd0f35305c1718519c43111
aman123956 <you@example.com>
2021-06-21 22:27:00 +0530

    Added file layout

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -46,11 +46,46 @@ function Home() {
                         Starred
                     </button>
                 </div>
-            </div>
+           
             {/* Right Part */}
             <div className={styles.rightBox}>
                 {/* Files To Show */}
-            </div>
+                <div className="row p-3">
+                    {/* One File */}
+                        <a href="https://www.amandixit.me" className="col-2 shadow mx-2 my-2">
+                            
+                            <img className={styles.fileImg} alt="file_img"
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbAVXZhlM-qA4bg_bOHRutl3wQVQdAbSWx_A&usqp=CAU" />
+                              <h4 className={styles.fileHeading}>My File1</h4>
+                        </a>
+                        <a href="https://drive-strike.netlify.app/" className="col-2 shadow mx-2 my-2">
+                            <img className={styles.fileImg} alt="file_img"
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbAVXZhlM-qA4bg_bOHRutl3wQVQdAbSWx_A&usqp=CAU" />
+                            <h4 className={styles.fileHeading}>My File2</h4>
+                        </a>
+                        <a href="https://drive-strike.netlify.app/" className="col-2 shadow mx-2 my-2">
+                            <img className={styles.fileImg} alt="file_img"
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbAVXZhlM-qA4bg_bOHRutl3wQVQdAbSWx_A&usqp=CAU" />
+                              <h4 className={styles.fileHeading}>My File3</h4>
+                        </a>
+                        <a href="https://drive-strike.netlify.app/" className="col-2 shadow mx-2 my-2">
+                            <img className={styles.fileImg}
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbAVXZhlM-qA4bg_bOHRutl3wQVQdAbSWx_A&usqp=CAU" />
+                              <h4 className={styles.fileHeading}>My File3</h4>
+                        </a>
+                        <a href="https://drive-strike.netlify.app/" className="col-2 shadow mx-2 my-2">
+                            <img className={styles.fileImg}
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbAVXZhlM-qA4bg_bOHRutl3wQVQdAbSWx_A&usqp=CAU" />
+                              <h4 className={styles.fileHeading}>My File3</h4>
+                        </a>
+                        <a href="https://drive-strike.netlify.app/" className="col-2 shadow mx-2 my-2">
+                            <img className={styles.fileImg}
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbAVXZhlM-qA4bg_bOHRutl3wQVQdAbSWx_A&usqp=CAU" />
+                              <h4 className={styles.fileHeading}>My File3</h4>
+                        </a>
+                </div>
+                </div>
+                </div>
         </div>
     )
 }
