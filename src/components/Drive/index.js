de7df341f1689054ee57d551488bee456cfebcf3
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-06-28 17:43:59 +0530

    :

--- src/components/Drive/index.js
+++ src/components/Drive/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link, useHistory} from 'react-router-dom'
 import styles from './styles.module.css'
 import Navbar from '../Navbar/index'
@@ -10,13 +10,20 @@ import { useDispatch, useSelector } from 'react-redux'
 import { setFolder } from '../../actions/currentFolderAction'
 import axios from 'axios'
 import url from '../../utilities'
+import Message from '../Message'
+import Loader1 from '../Loader/Loader-1'
 function Drive() {
+
     const history=useHistory()
     const dispatch=useDispatch()
-    const currentFolder=useSelector(state=>state.currentFolder)
+    const {currentFolder}=useSelector(state=>state.currentFolder)
 
     const userLogin=useSelector(state=>state.userLogin)
     const {userInfo}=userLogin
+    const [childFolder, setchildFolder] = useState(null)
+    const [childFiles, setchildFiles] = useState(null)
+    const [error, seterror] = useState(null)
+    const [loading, setloading] = useState(false)
 
     const setCurrentFolderDrive=async ()=>{
         const config = {
@@ -31,8 +38,35 @@ function Drive() {
             dispatch(setFolder(String(data.data._id)))
         }
     }
+    setCurrentFolderDrive()
 
     useEffect(() => {
+        console.log(currentFolder)
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:`Bearer ${userInfo.token}` 
+            }
+        }
+        const getFolderandFiles=async()=>{
+            try{
+                if(currentFolder){
+                    setloading(true)
+                    const {data}=await axios.get(`http://localhost:5000/api/folders/details/${currentFolder}`,config)
+                    setloading(false)
+                    if(data.success){
+                        setchildFiles(data.data.childFiles)
+                        setchildFolder(data.data.childFolder)
+                    }else{
+                        seterror('Some Error Occured')
+                    }
+                }
+            }catch(e){
+                console.log(e)
+                seterror('Some Error Occured')
+            }
+        }
+        getFolderandFiles()
     }, [dispatch,currentFolder])
 
     return (
@@ -46,7 +80,7 @@ function Drive() {
                     {/* Add File/Folder */}
                     <Modals />
                     <br />
-                    <Link to="/drive" className={styles.optionBtn} onClick={setCurrentFolderDrive}>
+                    <Link to="/drive" className={styles.optionBtn} >
                         My Drive
                     </Link>
                     <button className={styles.optionBtn}>
@@ -69,8 +103,14 @@ function Drive() {
                 <div className="mt-4"><Button className={styles.backButton} onClick={()=>history.push('/home')}>Back</Button></div>
                 <div className="row p-3">
                     {/* One File */}
-                    <h2>Drive</h2>
-                    <img src={emptyImg} className={styles.emptyImg} alt="empty_img" />
+                    {error && <Message variant={'danger'}>{error}</Message>}
+                    {loading && <Loader1></Loader1>}
+                    {
+                        childFiles&&<div></div>
+                    }
+                    {
+                        childFolder&&<div>Folder</div>
+                    }
                 </div>
             </div>
            </div>
