971d405dcb4afdbcf0249b02919abf2ee331fe0d
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-07-07 22:16:41 +0530

    Updated File Layout

--- src/components/File/index.js
+++ src/components/File/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import styles from './styles.module.css'
-function File({name,id}) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileDownload} from '@fortawesome/free-solid-svg-icons'
+import { Dropdown, DropdownButton } from 'react-bootstrap'
+function File({ name, id }) {
     return (
         <>
+
+
             <div className={styles.file}>
-                <div className={styles.icon}></div>
+                <div class={styles.header}>
+
+                    <DropdownButton id="dropdown-basic-button"  title="Choose Option">
+                        <Dropdown.Item href="#/action-1" type="button">Copy</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Delete</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+                <div className={styles.icon}>
+                    <FontAwesomeIcon icon={faFileDownload}
+                        className={styles.fIcon} />
+                </div>
                 <div className={styles.name}>{name}</div>
             </div>
+
         </>
     )
 }
