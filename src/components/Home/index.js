1128ca35d5f1d0f6ebf431fe6a30f31f7b5917fb
Abhinav <thakurabhinav17122001@gmail.com>
2022-02-03 16:46:49 +0530

    :

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,8 +2,6 @@ import React, { useEffect } from "react";
 import styles from "./styles.module.css";
 import Navbar from "../Navbar/index";
 import { Link, useHistory } from "react-router-dom";
-import { useSelector } from "react-redux";
-// import Modals from '../Modal/index'
 import emptyImg from "../../assets/img/empty.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -14,48 +12,22 @@ import {
 	faSdCard,
 } from "@fortawesome/free-solid-svg-icons";
 
-// import { useDispatch } from 'react-redux'
-
 function Home() {
 	const history = useHistory();
-	// const dispatch = useDispatch()
-
-	const userLogin = useSelector((state) => state.userLogin);
-	const { userInfo } = userLogin;
 
 	useEffect(() => {
+		const localData = localStorage.getItem("driveUserInfo");
+		const userInfo = localData ? JSON.parse(localData) : null;
 		if (!userInfo) {
 			history.push("/");
 		}
 		// eslint-disable-next-line
-	}, [userInfo]);
-
-	// const setCurrentFolderDrive=async ()=>{
-
-	//     console.log('Set folder called')
-	//     const config = {
-	//         headers: {
-	//         'Content-Type': 'application/json',
-	//         Authorization:`Bearer ${userInfo.token}`,
-	//         },
-	//     }
-	//     const {data}=await axios.get(`${url}/api/folders/${userInfo._id}`,config)
-	//     if(data.success)
-	//     {
-	//         dispatch(setFolder(String(data.data._id)))
-	//     }
-	// }
-
+	}, []);
 	return (
 		<div className="">
-			{/* Navbar */}
 			<Navbar />
-			{/* Main Content(20:80) */}
 			<div className="my-2 d-flex justify-content-flex-start">
-				{/* Left Part(20%) */}
 				<div className={styles.leftSideBar}>
-					{/* Add File/Folder */}
-					{/* <Modals /> */}
 					<br />
 					<br />
 					<Link to="/home" className={styles.optionBtn}>
@@ -79,11 +51,8 @@ function Home() {
 						&nbsp;&nbsp; Bin
 					</Link>
 				</div>
-				{/* Right Part */}
 				<div className={styles.rightBox}>
-					{/* Files To Show */}
 					<div className="row p-3">
-						{/* One File */}
 						<h2>Recent</h2>
 						<img
 							src={emptyImg}
--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,52 +1,63 @@
-import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstants";
+import {
+	USER_DETAILS_FAIL,
+	USER_DETAILS_REQUEST,
+	USER_DETAILS_SUCCESS,
+	USER_LOGIN_FAIL,
+	USER_LOGIN_REQUEST,
+	USER_LOGIN_SUCCESS,
+	USER_LOGOUT,
+	USER_REGISTER_FAIL,
+	USER_REGISTER_REQUEST,
+	USER_REGISTER_SUCCESS,
+} from "../constants/userConstants";
 
-export const userLoginReducer=(state={},action)=>{
-    switch (action.type){
-        case USER_LOGIN_REQUEST:
-            return {loading:true}
-        
-        case USER_LOGIN_SUCCESS:
-            return {loading:false,userInfo:action.payload}
-        
-        case USER_LOGIN_FAIL:
-            return {loading:false,error:action.payload}
-        
-        case USER_LOGOUT:
-            return {}
+export const userLoginReducer = (state = {}, action) => {
+	switch (action.type) {
+		case USER_LOGIN_REQUEST:
+			return { loading: true };
 
-        default:
-            return state
-    }
-}
+		case USER_LOGIN_SUCCESS:
+			return { loading: false, userInfo: action.payload };
 
-export const userRegisterReducer=(state={},action)=>{
-    switch (action.type){
-        case USER_REGISTER_REQUEST:
-            return {loading:true}
-        
-        case USER_REGISTER_SUCCESS:
-            return {loading:false,userInfo:action.payload}
-        
-        case USER_REGISTER_FAIL:
-            return {loading:false,error:action.payload}
-        
-        default:
-            return state
-    }
-}
+		case USER_LOGIN_FAIL:
+			return { loading: false, error: action.payload };
 
-export const userDetailReducer=(state={user:{}},action)=>{
-    switch (action.type){
-        case USER_DETAILS_REQUEST:
-            return {loading:true}
-        
-        case USER_DETAILS_SUCCESS:
-            return {loading:false,user:action.payload}
-        
-        case USER_DETAILS_FAIL:
-            return {loading:false,error:action.payload}
-        
-        default:
-            return state
-    }
-}
+		case USER_LOGOUT:
+			return {};
+
+		default:
+			return state;
+	}
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+	switch (action.type) {
+		case USER_REGISTER_REQUEST:
+			return { loading: true };
+
+		case USER_REGISTER_SUCCESS:
+			return { loading: false, userInfo: action.payload };
+
+		case USER_REGISTER_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+export const userDetailReducer = (state = { user: {} }, action) => {
+	switch (action.type) {
+		case USER_DETAILS_REQUEST:
+			return { loading: true };
+
+		case USER_DETAILS_SUCCESS:
+			return { loading: false, user: action.payload };
+
+		case USER_DETAILS_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+};
